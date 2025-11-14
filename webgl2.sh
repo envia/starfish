7867ea30b7fb1af0323db52cc34b071f27c1063e
Joonhyung Hwang <jh1984.hwang@samsung.com>
2025-11-14 15:13:08 +0900

    Update webgl2.sh

--- webgl2.sh
+++ webgl2.sh
@@ -1,4 +1,7 @@
-#git submodule update --init --recursive
+if [ ! -f binding_generator/scripts/starfish_code_generator.py ]
+then
+	git submodule update --init --recursive
+fi
 rm -rf out/webgl2
 cmake -Bout/webgl2 -DMODE=debug -DHOST=linux -DARCH=x64 -DBACKEND=uv_cairo_gl -DSHELL=glfw -DWEBGL=1 -G Ninja
 ninja -C out/webgl2 starfish.executable
