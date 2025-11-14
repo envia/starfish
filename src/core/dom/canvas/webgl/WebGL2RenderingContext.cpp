10557c81ff98a80fd686fce5e483aeb4053751cc
Joonhyung Hwang <jh1984.hwang@samsung.com>
2025-11-14 14:11:48 +0900

    Add WebGL2RenderingContext
    DONE:
    $ ./Starfish webgl2.html
    
    TODO:
    $ ./Starfish get.webgl.org/webgl2/index.html
    WebGLRenderingContext.cpp: getParameter(1200) > Unsupported pname: 0x0000(Escargot::ValueRef* Starfish::WebGLRenderingContext::getParameter(GLenum))

---
+++ src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019-present Samsung Electronics Co., Ltd
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ */
+
+#if defined(STARFISH_ENABLE_CANVAS) && defined(STARFISH_ENABLE_WEBGL)
+
+#include "StarfishConfig.h"
+#include "core/dom/canvas/webgl/WebGL2RenderingContext.h"
+#include "core/dom/ExecutionContext.h"
+
+namespace Starfish {
+
+WebGL2RenderingContext::WebGL2RenderingContext(HTMLCanvasElement* canvasElement)
+    : WebGLRenderingContext(canvasElement)
+{
+    // INDIGO_TODO
+}
+
+WebGL2RenderingContext::~WebGL2RenderingContext()
+{
+}
+
+ScriptBindingInstance* WebGL2RenderingContext::scriptBindingInstance()
+{
+    return executionContext()->scriptBindingInstance();
+}
+
+} // namespace Starfish
+
+#endif // defined(STARFISH_ENABLE_CANVAS) && defined(STARFISH_ENABLE_WEBGL)
