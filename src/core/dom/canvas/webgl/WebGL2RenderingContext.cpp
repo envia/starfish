096b369d5487da1c4c24b61ef987f2bca76a5a4a
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-01-07 13:35:58 +0900

    Implement WebGL2RenderingContext::getUniformBlockIndex()

--- src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
+++ src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
@@ -1894,8 +1894,13 @@ ScriptValue WebGL2RenderingContext::getActiveUniforms(
 GLuint WebGL2RenderingContext::getUniformBlockIndex(WebGLProgram* program,
                                                     String* uniformBlockName)
 {
-    STARFISH_UNIMPLEMENTED("WebGL2RenderingContextBase");
-    return 0;
+    ENTER_CONTEXT_SCOPE(GL_INVALID_INDEX);
+
+    if (program->context() != this) {
+        setGLError(GL_INVALID_OPERATION);
+        return GL_INVALID_INDEX;
+    }
+    return glGetUniformBlockIndex(program->glObject(), CSTR(uniformBlockName));
 }
 
 ScriptValue WebGL2RenderingContext::getActiveUniformBlockParameter(
