416f2ffaf9ed712871fde4ff19e4a6c046674ecf
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-01-12 09:32:48 +0900

    Add more checks to getActive{Attrib,Uniform}() of WebGL
    Signed-off-by: Joonhyung Hwang <jh1984.hwang@samsung.com>

--- src/core/dom/canvas/webgl/WebGLRenderingContext.cpp
+++ src/core/dom/canvas/webgl/WebGLRenderingContext.cpp
@@ -1260,6 +1260,10 @@ WebGLActiveInfo* WebGLRenderingContext::getActiveAttrib(WebGLProgram* program,
     m_gl->getProgramiv(program->glObject(), GL_ACTIVE_ATTRIBUTE_MAX_LENGTH,
                        &maxNameLength);
 
+    if (hasGLError()) {
+        return nullptr;
+    }
+
     GLint size;
     GLenum type;
     GLsizei length;
@@ -1293,6 +1297,10 @@ WebGLActiveInfo* WebGLRenderingContext::getActiveUniform(WebGLProgram* program,
     GLint maxNameLength;
     m_gl->getProgramiv(program->glObject(), GL_ACTIVE_UNIFORM_MAX_LENGTH,
                        &maxNameLength);
+
+    if (hasGLError()) {
+        return nullptr;
+    }
 
     GLint size;
     GLenum type;
