8dce25ea70c1898d48299fc4210c444b3c6333db
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-01-14 14:58:07 +0900

    Implement getActiveUniforms() of WebGL2
    Signed-off-by: Joonhyung Hwang <jh1984.hwang@samsung.com>

--- src/platform/canvas/gl/GenericGL.cpp
+++ src/platform/canvas/gl/GenericGL.cpp
@@ -1007,6 +1007,14 @@ public:
         glUniform4uiv(location, count, value);
     }
 
+    void getActiveUniformsiv(GLuint program, GLsizei uniformCount,
+                             const GLuint *uniformIndices, GLenum pname,
+                             GLint *params) override
+    {
+        glGetActiveUniformsiv(program, uniformCount, uniformIndices, pname,
+                              params);
+    }
+
     GLsync fenceSync(GLenum condition, GLbitfield flags) override
     {
         return glFenceSync(condition, flags);
