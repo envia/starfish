8ebd25392d19522e145a0ba23a0cd6773f8f877c
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-01-14 14:58:07 +0900

    Implement getActiveUniforms() of WebGL2
    Signed-off-by: Joonhyung Hwang <jh1984.hwang@samsung.com>

--- src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
+++ src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
@@ -986,6 +986,66 @@ ScriptValue WebGL2RenderingContext::getSyncParameter(WebGLSync* sync,
     return scriptNull();
 }
 
+ScriptValue WebGL2RenderingContext::getActiveUniforms(
+    WebGLProgram* program, GCAtomicVector<GLuint> uniformIndices, GLenum pname)
+{
+    ENTER_CONTEXT_SCOPE(scriptNull());
+
+    if (isFromCurrentContext(program)) {
+        setGLError(GL_INVALID_OPERATION);
+        return scriptNull();
+    }
+    switch (pname) {
+    // sequence<GLboolean>
+    case GL_UNIFORM_IS_ROW_MAJOR: {
+        GLsizei count = uniformIndices.size();
+        std::vector<GLint> values(count);
+        gl()->getActiveUniformsiv(program->glObject(), count,
+                                  uniformIndices.data(), pname, values.data());
+        return createScriptValue(
+            createArray(scriptBindingInstance(),
+                        std::vector<bool>(values.begin(), values.end())));
+    }
+    // sequence<GLenum>
+    case GL_UNIFORM_TYPE: {
+        GLsizei count = uniformIndices.size();
+        std::vector<GLint> values(count);
+        gl()->getActiveUniformsiv(program->glObject(), count,
+                                  uniformIndices.data(), pname, values.data());
+        return createScriptValue(
+            createTypedArray<Escargot::Uint32ArrayObjectRef>(
+                scriptBindingInstance(),
+                std::vector<GLenum>(values.begin(), values.end())));
+    }
+    // sequence<GLint>
+    case GL_UNIFORM_ARRAY_STRIDE:
+    case GL_UNIFORM_BLOCK_INDEX:
+    case GL_UNIFORM_MATRIX_STRIDE:
+    case GL_UNIFORM_OFFSET: {
+        GLsizei count = uniformIndices.size();
+        std::vector<GLint> values(count);
+        gl()->getActiveUniformsiv(program->glObject(), count,
+                                  uniformIndices.data(), pname, values.data());
+        return createScriptValue(
+            createTypedArray<Escargot::Int32ArrayObjectRef>(
+                scriptBindingInstance(), values));
+    }
+    // sequence<GLuint>
+    case GL_UNIFORM_SIZE: {
+        GLsizei count = uniformIndices.size();
+        std::vector<GLint> values(count);
+        gl()->getActiveUniformsiv(program->glObject(), count,
+                                  uniformIndices.data(), pname, values.data());
+        return createScriptValue(
+            createTypedArray<Escargot::Uint32ArrayObjectRef>(
+                scriptBindingInstance(),
+                std::vector<GLenum>(values.begin(), values.end())));
+    }
+    }
+    setGLError(GL_INVALID_ENUM);
+    return scriptNull();
+}
+
 WebGLVertexArrayObject* WebGL2RenderingContext::createVertexArray()
 {
     ENTER_CONTEXT_SCOPE(nullptr);
