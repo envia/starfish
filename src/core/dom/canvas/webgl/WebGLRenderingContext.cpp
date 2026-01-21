722bc5ee1fcd7f3ca09e33debf5c12a4c3edce2e
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-01-21 16:36:53 +0900

    Update getParameter()

--- src/core/dom/canvas/webgl/WebGLRenderingContext.cpp
+++ src/core/dom/canvas/webgl/WebGLRenderingContext.cpp
@@ -1075,37 +1075,6 @@ ScriptValue WebGLRenderingContext::getParameter(GLenum pname)
     ENTER_CONTEXT_SCOPE(scriptNull());
 
     switch (pname) {
-    // GLint
-    case GL_ALPHA_BITS:
-    case GL_BLUE_BITS:
-    case GL_DEPTH_BITS:
-    case GL_GREEN_BITS:
-    case GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS:
-    case GL_MAX_CUBE_MAP_TEXTURE_SIZE:
-    case GL_MAX_FRAGMENT_UNIFORM_VECTORS:
-    case GL_MAX_RENDERBUFFER_SIZE:
-    case GL_MAX_TEXTURE_IMAGE_UNITS:
-    case GL_MAX_TEXTURE_SIZE:
-    case GL_MAX_VARYING_VECTORS:
-    case GL_MAX_VERTEX_ATTRIBS:
-    case GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS:
-    case GL_MAX_VERTEX_UNIFORM_VECTORS:
-    case GL_PACK_ALIGNMENT:
-    case GL_RED_BITS:
-    case GL_SAMPLE_BUFFERS:
-    case GL_SAMPLES:
-    case GL_STENCIL_BACK_REF:
-    case GL_STENCIL_BITS:
-    case GL_STENCIL_CLEAR_VALUE:
-    case GL_STENCIL_REF:
-    case GL_SUBPIXEL_BITS:
-    case GL_MAX_SAMPLES:
-    case GL_UNPACK_ALIGNMENT: {
-        std::vector<int> values(1);
-        m_gl->getIntegerv(pname, &values[0]);
-        return ValueRef::create(values[0]);
-    }
-    // GLfloat
     case GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT: {
         if (!isExtensionEnabled("EXT_texture_filter_anisotropic")) {
             setGLError(GL_INVALID_ENUM);
@@ -1115,37 +1084,7 @@ ScriptValue WebGLRenderingContext::getParameter(GLenum pname)
         gl()->getFloatv(pname, &values[0]);
         return createScriptValue(values[0]);
     }
-    // WebGLProgram
-    case GL_CURRENT_PROGRAM: {
-        GLint value = -1;
-        m_gl->getIntegerv(pname, &value);
-
-        Optional<WebGLProgram*> maybe = m_state->webGLProgram();
-        if (!maybe.hasValue() || maybe.value()->isDeleted()) {
-            return scriptNull();
-        }
-        STARFISH_ASSERT(static_cast<GLint>(maybe.value()->glObject()) == value);
-        return maybe.value()->scriptValue();
-    }
-    // WebGLFramebuffer
-    case GL_FRAMEBUFFER_BINDING: {
-        GLint value = -1;
-        m_gl->getIntegerv(pname, &value);
-
-        if (isDefaultFramebufferBound()) {
-            return scriptNull();
-        }
-
-        Optional<WebGLFramebuffer*> maybe = m_state->webGLFramebuffer();
-        if (!maybe.hasValue() || maybe.value()->isDeleted()) {
-            return scriptNull();
-        }
-        TRACE(WEBGL, KV(maybe.value()->glObject()), KV(value));
-        STARFISH_ASSERT(static_cast<GLint>(maybe.value()->glObject()) == value);
-        return maybe.value()->scriptValue();
-    }
-    // WebGLVertexArrayObjectOES
-    case GL_VERTEX_ARRAY_BINDING: {
+    case GL_VERTEX_ARRAY_BINDING: /* WebGL2? */ {
         // GL_VERTEX_ARRAY_BINDING_OES
         if (!isExtensionEnabled("OES_vertex_array_object")) {
             setGLError(GL_INVALID_ENUM);
@@ -1167,34 +1106,31 @@ ScriptValue WebGLRenderingContext::getParameter(GLenum pname)
         STARFISH_ASSERT(static_cast<GLint>(maybe.value()->glObject()) == value);
         return maybe.value()->scriptValue();
     }
-    // WebGLBuffer
-    case GL_ARRAY_BUFFER_BINDING:
-    case GL_ELEMENT_ARRAY_BUFFER_BINDING: {
-        GLuint target = (pname == GL_ARRAY_BUFFER_BINDING)
-                            ? GL_ARRAY_BUFFER
-                            : GL_ELEMENT_ARRAY_BUFFER_BINDING;
-        WebGLBuffer* buffer = m_state->getBoundBuffer(target).valueOr(nullptr);
-        return buffer ? buffer->scriptValue() : scriptNull();
-    }
-    // GLenum
-    case kIMPLEMENTATION_COLOR_READ_TYPE: {
-        // Our implementation-chosen is a combination of RGBA and UNSIGNED_BYTE.
-        return ValueRef::create(GL_UNSIGNED_BYTE);
-    }
-    case kIMPLEMENTATION_COLOR_READ_FORMAT: {
-        return ValueRef::create(GL_RGBA);
-    }
     // DOMString
-    case GL_SHADING_LANGUAGE_VERSION:
+    case GL_RENDERER: {
+        const std::string output =
+            reinterpret_cast<const char*>(glGetString(pname));
+        return StringRef::createFromASCII(output.c_str(), output.length());
+    }
+    case GL_SHADING_LANGUAGE_VERSION: /* WebGL2 */
         return createScriptASCIIString(kShadingLanguageVersion);
-    case GL_VERSION:
-        return createScriptASCIIString(kVersion);
-    case GL_RENDERER:
     case GL_VENDOR: {
         const std::string output =
             reinterpret_cast<const char*>(glGetString(pname));
         return StringRef::createFromASCII(output.c_str(), output.length());
     }
+    case GL_VERSION: /* WebGL2 */
+        return createScriptASCIIString(kVersion);
+    // Float32Array (with 2 elements)
+    case GL_ALIASED_LINE_WIDTH_RANGE:
+    case GL_ALIASED_POINT_SIZE_RANGE:
+    case GL_DEPTH_RANGE:
+    // Float32Array (with 4 values)
+    case GL_BLEND_COLOR:
+    case GL_COLOR_CLEAR_VALUE:
+        STARFISH_UNIMPLEMENTED("WebGLRenderingContext::getParameter");
+        STARFISH_UNSUPPORTED("pname: 0x%04X(%s)", pname, __PRETTY_FUNCTION__);
+        return scriptNull();
     // GLboolean
     case GL_BLEND:
     case GL_CULL_FACE:
@@ -1211,12 +1147,88 @@ ScriptValue WebGLRenderingContext::getParameter(GLenum pname)
         m_gl->getBooleanv(pname, &values[0]);
         return createScriptValue(static_cast<bool>(values[0]));
     }
-    case kUNPACK_FLIP_Y_WEBGL: {
+    case kUNPACK_FLIP_Y_WEBGL:
         return createScriptValue(m_unpackFlipY);
-    }
-    case kUNPACK_PREMULTIPLY_ALPHA_WEBGL: {
+    case kUNPACK_PREMULTIPLY_ALPHA_WEBGL:
         return createScriptValue(m_unpackPremultiplyAlpha);
+    // GLenum
+    case GL_ACTIVE_TEXTURE:
+    case GL_BLEND_DST_ALPHA:
+    case GL_BLEND_DST_RGB:
+    case GL_BLEND_EQUATION_ALPHA:
+    case GL_BLEND_EQUATION_RGB:
+    case GL_BLEND_SRC_ALPHA:
+    case GL_BLEND_SRC_RGB:
+    case GL_CULL_FACE_MODE:
+    case GL_DEPTH_FUNC:
+    case GL_FRONT_FACE:
+    case GL_GENERATE_MIPMAP_HINT:
+        STARFISH_UNIMPLEMENTED("WebGLRenderingContext::getParameter");
+        STARFISH_UNSUPPORTED("pname: 0x%04X(%s)", pname, __PRETTY_FUNCTION__);
+        return scriptNull();
+    case GL_IMPLEMENTATION_COLOR_READ_FORMAT:
+        // kIMPLEMENTATION_COLOR_READ_FORMAT
+        return ValueRef::create(GL_RGBA);
+    case GL_IMPLEMENTATION_COLOR_READ_TYPE:
+        // kIMPLEMENTATION_COLOR_READ_TYPE
+        // Our implementation-chosen is a combination of RGBA and UNSIGNED_BYTE.
+        return ValueRef::create(GL_UNSIGNED_BYTE);
+    case GL_STENCIL_BACK_FAIL:
+    case GL_STENCIL_BACK_FUNC:
+    case GL_STENCIL_BACK_PASS_DEPTH_FAIL:
+    case GL_STENCIL_BACK_PASS_DEPTH_PASS:
+    case GL_STENCIL_FAIL:
+    case GL_STENCIL_FUNC:
+    case GL_STENCIL_PASS_DEPTH_FAIL:
+    case GL_STENCIL_PASS_DEPTH_PASS:
+    case kUNPACK_COLORSPACE_CONVERSION_WEBGL:
+    // GLfloat
+    case GL_DEPTH_CLEAR_VALUE:
+    case GL_LINE_WIDTH:
+    case GL_POLYGON_OFFSET_FACTOR:
+    case GL_POLYGON_OFFSET_UNITS:
+    case GL_SAMPLE_COVERAGE_VALUE:
+        STARFISH_UNIMPLEMENTED("WebGLRenderingContext::getParameter");
+        STARFISH_UNSUPPORTED("pname: 0x%04X(%s)", pname, __PRETTY_FUNCTION__);
+        return scriptNull();
+    // GLint
+    case GL_ALPHA_BITS: /* WebGL2 */
+    case GL_BLUE_BITS:  /* WebGL2 */
+    case GL_DEPTH_BITS:
+    case GL_GREEN_BITS: /* WebGL2 */
+    case GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS:
+    case GL_MAX_CUBE_MAP_TEXTURE_SIZE:
+    case GL_MAX_FRAGMENT_UNIFORM_VECTORS:
+    case GL_MAX_RENDERBUFFER_SIZE:
+    case GL_MAX_SAMPLES: /* WebGL2? */
+    case GL_MAX_TEXTURE_IMAGE_UNITS:
+    case GL_MAX_TEXTURE_SIZE:
+    case GL_MAX_VARYING_VECTORS:
+    case GL_MAX_VERTEX_ATTRIBS:
+    case GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS:
+    case GL_MAX_VERTEX_UNIFORM_VECTORS:
+    case GL_PACK_ALIGNMENT:
+    case GL_RED_BITS: /* WebGL2 */
+    case GL_SAMPLES:
+    case GL_SAMPLE_BUFFERS:
+    case GL_STENCIL_BACK_REF:
+    case GL_STENCIL_BITS:
+    case GL_STENCIL_CLEAR_VALUE:
+    case GL_STENCIL_REF:
+    case GL_SUBPIXEL_BITS:
+    case GL_UNPACK_ALIGNMENT: {
+        std::vector<int> values(1);
+        m_gl->getIntegerv(pname, &values[0]);
+        return ValueRef::create(values[0]);
     }
+    // GLuint
+    case GL_STENCIL_BACK_VALUE_MASK:
+    case GL_STENCIL_BACK_WRITEMASK:
+    case GL_STENCIL_VALUE_MASK:
+    case GL_STENCIL_WRITEMASK:
+        STARFISH_UNIMPLEMENTED("WebGLRenderingContext::getParameter");
+        STARFISH_UNSUPPORTED("pname: 0x%04X(%s)", pname, __PRETTY_FUNCTION__);
+        return scriptNull();
     // Int32Array (with 2 elements)
     case GL_MAX_VIEWPORT_DIMS: {
         std::vector<int> values(2);
@@ -1239,6 +1251,54 @@ ScriptValue WebGLRenderingContext::getParameter(GLenum pname)
         return createTypedArray<Int32ArrayObjectRef>(scriptBindingInstance(),
                                                      std::vector<int>());
     }
+    // WebGLBuffer
+    case GL_ARRAY_BUFFER_BINDING:
+    case GL_ELEMENT_ARRAY_BUFFER_BINDING: {
+        GLuint target = (pname == GL_ARRAY_BUFFER_BINDING)
+                            ? GL_ARRAY_BUFFER
+                            : GL_ELEMENT_ARRAY_BUFFER_BINDING;
+        WebGLBuffer* buffer = m_state->getBoundBuffer(target).valueOr(nullptr);
+        return buffer ? buffer->scriptValue() : scriptNull();
+    }
+    // WebGLFramebuffer
+    case GL_FRAMEBUFFER_BINDING: /* WebGL2 (GL_DRAW_FRAMEBUFFER_BINDING) */ {
+        GLint value = -1;
+        m_gl->getIntegerv(pname, &value);
+
+        if (isDefaultFramebufferBound()) {
+            return scriptNull();
+        }
+
+        Optional<WebGLFramebuffer*> maybe = m_state->webGLFramebuffer();
+        if (!maybe.hasValue() || maybe.value()->isDeleted()) {
+            return scriptNull();
+        }
+        TRACE(WEBGL, KV(maybe.value()->glObject()), KV(value));
+        STARFISH_ASSERT(static_cast<GLint>(maybe.value()->glObject()) == value);
+        return maybe.value()->scriptValue();
+    }
+    // WebGLProgram
+    case GL_CURRENT_PROGRAM: {
+        GLint value = -1;
+        m_gl->getIntegerv(pname, &value);
+
+        Optional<WebGLProgram*> maybe = m_state->webGLProgram();
+        if (!maybe.hasValue() || maybe.value()->isDeleted()) {
+            return scriptNull();
+        }
+        STARFISH_ASSERT(static_cast<GLint>(maybe.value()->glObject()) == value);
+        return maybe.value()->scriptValue();
+    }
+    // WebGLRenderbuffer
+    case GL_RENDERBUFFER_BINDING:
+    // WebGLTexture
+    case GL_TEXTURE_BINDING_2D:
+    case GL_TEXTURE_BINDING_CUBE_MAP:
+    // sequence<GLboolean> (with 4 values)
+    case GL_COLOR_WRITEMASK:
+        STARFISH_UNIMPLEMENTED("WebGLRenderingContext::getParameter");
+        STARFISH_UNSUPPORTED("pname: 0x%04X(%s)", pname, __PRETTY_FUNCTION__);
+        return scriptNull();
     default:
         STARFISH_UNSUPPORTED("pname: 0x%04X(%s)", pname, __PRETTY_FUNCTION__);
         return scriptNull();
