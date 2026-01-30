d2687173e15ede5311231f78a44b9f39d1380025
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-01-30 16:29:53 +0900

    Revert "Update list of test suites"
    This reverts commit 86c6e72f500ecc258b42302d91bfce55e5909719.

--- tool/test_runner.py
+++ tool/test_runner.py
@@ -301,8 +301,7 @@ if __name__ == "__main__":
     test_functions = []
     for key, value in list(locals().items()):
         if callable(value) and value.__module__ == __name__:
-            if key not in ["file_len", "print_columns", "print_table",
-                           "run_test", "run_vendor_test_khronos"]:
+            if key != "run_test" and key != "file_len":
                 test_functions.append(key)
     print_columns(sorted(test_functions), 4)
 
