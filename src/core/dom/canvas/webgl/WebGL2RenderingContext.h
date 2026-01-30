041e5079183569d5be7fe5dc0eec682928ad1062
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-01-30 16:21:29 +0900

    Revert "Implement getFragDataLocation() of WebGL2"
    This reverts commit fd84617fddb082b82d1693c24496a5895a494134.

--- src/core/dom/canvas/webgl/WebGL2RenderingContext.h
+++ src/core/dom/canvas/webgl/WebGL2RenderingContext.h
@@ -93,9 +93,6 @@ public:
 
     // Implement WebGL2RenderingContextBase
 
-    /* Programs and shaders */
-    GLint getFragDataLocation(WebGLProgram* program, String* name);
-
     /* Sync objects */
     Optional<WebGLSync*> fenceSync(GLenum condition, GLbitfield flags);
     GLboolean isSync(Optional<WebGLSync*> sync);
