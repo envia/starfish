2092f93d7b2fdcae762865c0022143c096b7cd1f
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-03-09 18:43:51 +0900

    Fix getVertexAttrib()
    DONE:
    https://registry.khronos.org/webgl/conformance-suites/1.0.3/conformance/extensions/oes-vertex-array-object-bufferData.html
    https://registry.khronos.org/webgl/conformance-suites/1.0.3/conformance/extensions/oes-vertex-array-object.html
    test/cairo/reftest/vendor/khronos/webgl/1.0.3/conformance/extensions/oes-vertex-array-object.html
    runAttributeTests()
    Starfish: .../src/core/dom/canvas/webgl/WebGLRenderingContext.cpp:1737: Escargot::ValueRef* Starfish::WebGLRenderingContext::getVertexAttrib(GLuint, GLenum): Assertion `index == maybeBuffer.value()->glObject()' failed.
    
    TODO:
    Console.cpp: operator()(251) > console.log: FAIL ELEMENT_ARRAY_BUFFER_BINDING not preserved

--- src/core/dom/canvas/webgl/WebGLRenderingContext.cpp
+++ src/core/dom/canvas/webgl/WebGLRenderingContext.cpp
@@ -1734,7 +1734,8 @@ ScriptValue WebGLRenderingContext::getVertexAttrib(GLuint index, GLenum pname)
 
         TRACE(WEBGL, KV(index), KV(maybeBuffer.value()->glObject()));
 
-        STARFISH_ASSERT(index == maybeBuffer.value()->glObject());
+        STARFISH_ASSERT(static_cast<GLuint>(value) ==
+                        maybeBuffer.value()->glObject());
 
         return maybeBuffer.value()->scriptValue();
     }
