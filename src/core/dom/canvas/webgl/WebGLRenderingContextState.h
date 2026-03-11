c4a786b6192d49def036f6bfb084c900d6c4f607
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-03-11 13:23:13 +0900

    Update WebGLRenderingContextState

--- src/core/dom/canvas/webgl/WebGLRenderingContextState.h
+++ src/core/dom/canvas/webgl/WebGLRenderingContextState.h
@@ -30,14 +30,12 @@ namespace Starfish {
 class WebGLBuffer;
 
 #define STATEFUL_VALUES(V)                                             \
-    V(WebGLVertexArrayObject, Optional<WebGLVertexArrayObject*>,       \
-      webGLVertexArrayObject)                                          \
-    V(WebGLVertexArrayObjectOES, Optional<WebGLVertexArrayObjectOES*>, \
-      webGLVertexArrayObjectOES)                                       \
     V(WebGLFramebuffer, Optional<WebGLFramebuffer*>, webGLFramebuffer) \
     V(WebGLProgram, Optional<WebGLProgram*>, webGLProgram)
 
 // Forward declarations
+class WebGLVertexArrayObject;
+class WebGLVertexArrayObjectOES;
 #define V(Constructor, _, __) class Constructor;
 STATEFUL_VALUES(V);
 #undef V
@@ -46,7 +44,24 @@ class WebGLRenderingContextState : public gc {
 public:
     WebGLRenderingContextState();
 
-// Define functions
+    // Define functions
+    Optional<WebGLVertexArrayObject*> webGLVertexArrayObject() const
+    {
+        return m_webGLVertexArrayObject;
+    }
+    void setWebGLVertexArrayObject(Optional<WebGLVertexArrayObject*> value)
+    {
+        m_webGLVertexArrayObject = value;
+    }
+    Optional<WebGLVertexArrayObjectOES*> webGLVertexArrayObjectOES() const
+    {
+        return m_webGLVertexArrayObjectOES;
+    }
+    void setWebGLVertexArrayObjectOES(
+        Optional<WebGLVertexArrayObjectOES*> value)
+    {
+        m_webGLVertexArrayObjectOES = value;
+    }
 #define V(Constructor, Type, MemberName)          \
     DEFINE_GETTER(Type, MemberName);              \
     DEFINE_SETTER(Type, MemberName, Constructor); \
@@ -67,6 +82,8 @@ public:
 
 private:
     // Define variables
+    Optional<WebGLVertexArrayObject*> m_webGLVertexArrayObject;
+    Optional<WebGLVertexArrayObjectOES*> m_webGLVertexArrayObjectOES;
 #define V(Constructor, Type, MemberName) Type m_##MemberName;
     STATEFUL_VALUES(V);
 #undef V
