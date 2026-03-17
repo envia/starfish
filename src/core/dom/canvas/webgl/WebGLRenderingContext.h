0e9bccb13e5608e0840a13b88ccda719508e72de
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-03-17 20:34:43 +0900

    Use updateGLError() in place of hasGLError()
    In most cases, we use hasGLError() just after a GL command to check for
    new errors from it.
    However, if there were GL errors before execution of the GL command,
    hasGLError() will return true regardless of the execution result.
    To avoid confusion, make updateGLError() return true when there is
    a new error and use it in place of hasGLError().
    
    Signed-off-by: Joonhyung Hwang <jh1984.hwang@samsung.com>

--- src/core/dom/canvas/webgl/WebGLRenderingContext.h
+++ src/core/dom/canvas/webgl/WebGLRenderingContext.h
@@ -311,7 +311,7 @@ public:
     // Use setGLError only on WebGLRenderingContext and GLExtensions.
     void setGLError(GLenum code, const char* message = nullptr);
     bool hasGLError();
-    void updateGLError();
+    bool updateGLError();
     bool executeInContextScope(std::function<void()> callback);
     WebGLRenderingContextState* getState();
 
