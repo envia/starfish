866196dd47f282eb313281f63bc7dd1e88cc021b
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-03-25 20:42:30 +0900

    Change order in getUniformImpl() of WebGL1

--- src/core/dom/canvas/webgl/WebGLRenderingContext.cpp
+++ src/core/dom/canvas/webgl/WebGLRenderingContext.cpp
@@ -1802,6 +1802,16 @@ Optional<ScriptValue> WebGLRenderingContext::getUniformImpl(
     ENTER_CONTEXT_SCOPE(Optional<ScriptValue>());
 
     switch (type) {
+    case GL_BOOL: {
+        GLint value;
+        m_gl->getUniformiv(program->glObject(), location->location(), &value);
+        return createScriptValue(static_cast<bool>(value));
+    }
+    case GL_INT: {
+        GLint value;
+        m_gl->getUniformiv(program->glObject(), location->location(), &value);
+        return createScriptValue(value);
+    }
     case GL_FLOAT: {
         GLfloat value;
         m_gl->getUniformfv(program->glObject(), location->location(), &value);
@@ -1814,50 +1824,12 @@ Optional<ScriptValue> WebGLRenderingContext::getUniformImpl(
         return createTypedArray<Float32ArrayObjectRef>(scriptBindingInstance(),
                                                        values);
     }
-    case GL_FLOAT_VEC3: {
-        std::vector<GLfloat> values(3);
-        m_gl->getUniformfv(program->glObject(), location->location(),
-                           &values[0]);
-        return createTypedArray<Float32ArrayObjectRef>(scriptBindingInstance(),
-                                                       values);
-    }
-    case GL_FLOAT_VEC4: {
-        std::vector<GLfloat> values(4);
-        m_gl->getUniformfv(program->glObject(), location->location(),
-                           &values[0]);
-        return createTypedArray<Float32ArrayObjectRef>(scriptBindingInstance(),
-                                                       values);
-    }
-    case GL_INT: {
-        GLint value;
-        m_gl->getUniformiv(program->glObject(), location->location(), &value);
-        return createScriptValue(value);
-    }
     case GL_INT_VEC2: {
         std::vector<GLint> values(2);
         m_gl->getUniformiv(program->glObject(), location->location(),
                            &values[0]);
         return createTypedArray<Int32ArrayObjectRef>(scriptBindingInstance(),
                                                      values);
-    }
-    case GL_INT_VEC3: {
-        std::vector<GLint> values(3);
-        m_gl->getUniformiv(program->glObject(), location->location(),
-                           &values[0]);
-        return createTypedArray<Int32ArrayObjectRef>(scriptBindingInstance(),
-                                                     values);
-    }
-    case GL_INT_VEC4: {
-        std::vector<GLint> values(4);
-        m_gl->getUniformiv(program->glObject(), location->location(),
-                           &values[0]);
-        return createTypedArray<Int32ArrayObjectRef>(scriptBindingInstance(),
-                                                     values);
-    }
-    case GL_BOOL: {
-        GLint value;
-        m_gl->getUniformiv(program->glObject(), location->location(), &value);
-        return createScriptValue(static_cast<bool>(value));
     }
     case GL_BOOL_VEC2: {
         std::vector<GLint> values(2);
@@ -1867,6 +1839,20 @@ Optional<ScriptValue> WebGLRenderingContext::getUniformImpl(
             createArray(scriptBindingInstance(),
                         std::vector<bool>(values.begin(), values.end())));
     }
+    case GL_FLOAT_VEC3: {
+        std::vector<GLfloat> values(3);
+        m_gl->getUniformfv(program->glObject(), location->location(),
+                           &values[0]);
+        return createTypedArray<Float32ArrayObjectRef>(scriptBindingInstance(),
+                                                       values);
+    }
+    case GL_INT_VEC3: {
+        std::vector<GLint> values(3);
+        m_gl->getUniformiv(program->glObject(), location->location(),
+                           &values[0]);
+        return createTypedArray<Int32ArrayObjectRef>(scriptBindingInstance(),
+                                                     values);
+    }
     case GL_BOOL_VEC3: {
         std::vector<GLint> values(3);
         m_gl->getUniformiv(program->glObject(), location->location(),
@@ -1874,6 +1860,20 @@ Optional<ScriptValue> WebGLRenderingContext::getUniformImpl(
         return createScriptValue(
             createArray(scriptBindingInstance(),
                         std::vector<bool>(values.begin(), values.end())));
+    }
+    case GL_FLOAT_VEC4: {
+        std::vector<GLfloat> values(4);
+        m_gl->getUniformfv(program->glObject(), location->location(),
+                           &values[0]);
+        return createTypedArray<Float32ArrayObjectRef>(scriptBindingInstance(),
+                                                       values);
+    }
+    case GL_INT_VEC4: {
+        std::vector<GLint> values(4);
+        m_gl->getUniformiv(program->glObject(), location->location(),
+                           &values[0]);
+        return createTypedArray<Int32ArrayObjectRef>(scriptBindingInstance(),
+                                                     values);
     }
     case GL_BOOL_VEC4: {
         std::vector<GLint> values(4);
