dbeabe322f61b294863c5832393c5fa3fa5fd973
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-04-06 11:12:55 +0900

    Implement WebGL2 vertex attributes

--- src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
+++ src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
@@ -41,6 +41,7 @@ static constexpr GLenum kMAX_CLIENT_WAIT_TIMEOUT_WEBGL = 0x9247;
 
 /* WebGL constants */
 static constexpr GLint64 kMaxClientWaitTimeoutWebgl = 0;
+static constexpr GLsizei kMaximumSupportedStride = 255;
 static constexpr char kShadingLanguageVersion[] = "WebGL GLSL ES 3.00";
 static constexpr char kVersion[] = "WebGL 2.0";
 
@@ -382,11 +383,32 @@ ScriptValue WebGL2RenderingContext::getVertexAttrib(GLuint index, GLenum pname)
     }
     // One of Float32Array, Int32Array or Uint32Array (each with 4 elements)
     case GL_CURRENT_VERTEX_ATTRIB: {
-        std::vector<float> values(4);
-        gl()->getVertexAttribfv(index, pname, &values[0]);
-        return createScriptValue(
-            createTypedArray<Escargot::Float32ArrayObjectRef>(
-                scriptBindingInstance(), values));
+        switch (m_currentVertexAttribType) {
+        case GL_INT: {
+            std::vector<int32_t> values(4);
+            gl()->getVertexAttribIiv(index, pname, &values[0]);
+            return createScriptValue(
+                createTypedArray<Escargot::Int32ArrayObjectRef>(
+                    scriptBindingInstance(), values));
+        }
+        case GL_UNSIGNED_INT: {
+            std::vector<uint32_t> values(4);
+            gl()->getVertexAttribIuiv(index, pname, &values[0]);
+            return createScriptValue(
+                createTypedArray<Escargot::Uint32ArrayObjectRef>(
+                    scriptBindingInstance(), values));
+        }
+        case GL_FLOAT: {
+            std::vector<float> values(4);
+            gl()->getVertexAttribfv(index, pname, &values[0]);
+            return createScriptValue(
+                createTypedArray<Escargot::Float32ArrayObjectRef>(
+                    scriptBindingInstance(), values));
+        }
+        default:
+            STARFISH_ASSERT_NOT_REACHED();
+            return scriptNull();
+        }
     }
     // WebGLBuffer
     case GL_VERTEX_ATTRIB_ARRAY_BUFFER_BINDING: {
@@ -435,6 +457,56 @@ GLint WebGL2RenderingContext::getFragDataLocation(WebGLProgram* program,
         return -1;
     }
     return gl()->getFragDataLocation(program->glObject(), CSTR(name));
+}
+
+void WebGL2RenderingContext::vertexAttrib1f(GLuint index, GLfloat x)
+{
+    WebGLRenderingContext::vertexAttrib1f(index, x);
+    m_currentVertexAttribType = GL_FLOAT;
+}
+
+void WebGL2RenderingContext::vertexAttrib2f(GLuint index, GLfloat x, GLfloat y)
+{
+    WebGLRenderingContext::vertexAttrib2f(index, x, y);
+    m_currentVertexAttribType = GL_FLOAT;
+}
+
+void WebGL2RenderingContext::vertexAttrib3f(GLuint index, GLfloat x, GLfloat y,
+                                            GLfloat z)
+{
+    WebGLRenderingContext::vertexAttrib3f(index, x, y, z);
+    m_currentVertexAttribType = GL_FLOAT;
+}
+
+void WebGL2RenderingContext::vertexAttrib4f(GLuint index, GLfloat x, GLfloat y,
+                                            GLfloat z, GLfloat w)
+{
+    WebGLRenderingContext::vertexAttrib4f(index, x, y, z, w);
+    m_currentVertexAttribType = GL_FLOAT;
+}
+
+void WebGL2RenderingContext::vertexAttrib1fv(GLuint index, Float32List values)
+{
+    WebGLRenderingContext::vertexAttrib1fv(index, values);
+    m_currentVertexAttribType = GL_FLOAT;
+}
+
+void WebGL2RenderingContext::vertexAttrib2fv(GLuint index, Float32List values)
+{
+    WebGLRenderingContext::vertexAttrib2fv(index, values);
+    m_currentVertexAttribType = GL_FLOAT;
+}
+
+void WebGL2RenderingContext::vertexAttrib3fv(GLuint index, Float32List values)
+{
+    WebGLRenderingContext::vertexAttrib3fv(index, values);
+    m_currentVertexAttribType = GL_FLOAT;
+}
+
+void WebGL2RenderingContext::vertexAttrib4fv(GLuint index, Float32List values)
+{
+    WebGLRenderingContext::vertexAttrib4fv(index, values);
+    m_currentVertexAttribType = GL_FLOAT;
 }
 
 void WebGL2RenderingContext::uniform1ui(
@@ -648,6 +720,128 @@ void WebGL2RenderingContext::uniformMatrix3x4fv(
 {
     implementUniformMatrixMxNfv(3, 4, &GL::uniformMatrix3x4fv, location,
                                 transpose, data, srcOffset, srcLength);
+}
+
+void WebGL2RenderingContext::vertexAttribI4i(GLuint index, GLint x, GLint y,
+                                             GLint z, GLint w)
+{
+    ENTER_CONTEXT_SCOPE();
+
+    gl()->vertexAttribI4i(index, x, y, z, w);
+    m_currentVertexAttribType = GL_INT;
+}
+
+void WebGL2RenderingContext::vertexAttribI4iv(GLuint index, Int32List values)
+{
+    ENTER_CONTEXT_SCOPE();
+
+    if (values.isInt32ArrayValue()) {
+        const ScriptInt32Array v = values.getInt32ArrayValue();
+        const size_t dataLength = v->arrayLength();
+        if (dataLength != 4) {
+            setGLError(GL_INVALID_VALUE);
+            return;
+        }
+        gl()->vertexAttribI4iv(index,
+                               reinterpret_cast<const GLint*>(v->rawBuffer()));
+    } else if (values.isSequenceOfGLintValue()) {
+        GCAtomicVector<int32_t> v = values.getSequenceOfGLintValue();
+        const size_t dataLength = v.size();
+        if (dataLength != 4) {
+            setGLError(GL_INVALID_VALUE);
+            return;
+        }
+        gl()->vertexAttribI4iv(index, v.data());
+    } else {
+        STARFISH_ASSERT_NOT_REACHED();
+    }
+    m_currentVertexAttribType = GL_INT;
+}
+
+void WebGL2RenderingContext::vertexAttribI4ui(GLuint index, GLuint x, GLuint y,
+                                              GLuint z, GLuint w)
+{
+    ENTER_CONTEXT_SCOPE();
+
+    gl()->vertexAttribI4ui(index, x, y, z, w);
+    m_currentVertexAttribType = GL_UNSIGNED_INT;
+}
+
+void WebGL2RenderingContext::vertexAttribI4uiv(GLuint index, Uint32List values)
+{
+    ENTER_CONTEXT_SCOPE();
+
+    if (values.isUint32ArrayValue()) {
+        const ScriptUint32Array v = values.getUint32ArrayValue();
+        const size_t dataLength = v->arrayLength();
+        if (dataLength != 4) {
+            setGLError(GL_INVALID_VALUE);
+            return;
+        }
+        gl()->vertexAttribI4uiv(
+            index, reinterpret_cast<const GLuint*>(v->rawBuffer()));
+    } else if (values.isSequenceOfGLuintValue()) {
+        GCAtomicVector<uint32_t> v = values.getSequenceOfGLuintValue();
+        const size_t dataLength = v.size();
+        if (dataLength != 4) {
+            setGLError(GL_INVALID_VALUE);
+            return;
+        }
+        gl()->vertexAttribI4uiv(index, v.data());
+    } else {
+        STARFISH_ASSERT_NOT_REACHED();
+    }
+    m_currentVertexAttribType = GL_UNSIGNED_INT;
+}
+
+void WebGL2RenderingContext::vertexAttribIPointer(GLuint index, GLint size,
+                                                  GLenum type, GLsizei stride,
+                                                  GLintptr offset)
+{
+    ENTER_CONTEXT_SCOPE();
+
+    switch (type) {
+    case GL_SHORT:
+    case GL_UNSIGNED_SHORT:
+        if (offset % 2 != 0 || stride % 2 != 0) {
+            setGLError(GL_INVALID_OPERATION);
+            return;
+        }
+        break;
+    case GL_INT:
+    case GL_UNSIGNED_INT:
+        if (offset % 4 != 0 || stride % 4 != 0) {
+            setGLError(GL_INVALID_OPERATION);
+            return;
+        }
+        break;
+    default:
+        break;
+    }
+
+    if (offset < 0) {
+        setGLError(GL_INVALID_VALUE);
+        return;
+    }
+
+    if (!getState()->getBoundBuffer(GL_ARRAY_BUFFER).hasValue() &&
+        offset != 0) {
+        setGLError(GL_INVALID_OPERATION);
+        return;
+    }
+
+    if (stride > kMaximumSupportedStride) {
+        setGLError(GL_INVALID_VALUE);
+        return;
+    }
+
+    gl()->vertexAttribIPointer(index, size, type, stride,
+                               reinterpret_cast<const void*>(offset));
+    if (hasNewGLError()) {
+        return;
+    }
+    getState()->setBufferBoundToVertexAttributes(
+        index, getState()->getBoundBuffer(GL_ARRAY_BUFFER));
 }
 
 Optional<WebGLSync*> WebGL2RenderingContext::fenceSync(GLenum condition,
