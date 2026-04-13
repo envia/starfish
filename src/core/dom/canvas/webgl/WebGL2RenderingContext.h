7c6977f4ed5788e98fb950306eab4a3f6a9fb122
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-04-13 09:58:26 +0900

    Add checkSrcData() for WebGL
    WebGL1 texImage2D() supports more ArrayBufferView types then WebGL2.
    
    Add a virtual method checkSrcData() to WebGLRenderingContext and
    override it in WebGLRenderingContext2 to handle the difference.
    
    Signed-off-by: Joonhyung Hwang <jh1984.hwang@samsung.com>

--- src/core/dom/canvas/webgl/WebGL2RenderingContext.h
+++ src/core/dom/canvas/webgl/WebGL2RenderingContext.h
@@ -266,6 +266,7 @@ protected:
 private:
     bool checkInternalFormat(GLint internalFormat, GLenum format,
                              GLenum type) override;
+    bool checkSrcData(ScriptArrayBufferView srcData, GLenum type) override;
 
     GLenum m_currentVertexAttribType = GL_FLOAT;
 };
