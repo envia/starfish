bb0ab3c53afaeb6d615f8d125c31476dcc0822e5
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-04-15 14:57:41 +0900

    Use isFromCurrentContext() in getUniform()
    Use isFromCurrentContext() in WebGL2RenderingContext::getUniform() for
    consistency.
    
    Signed-off-by: Joonhyung Hwang <jh1984.hwang@samsung.com>

--- src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
+++ src/core/dom/canvas/webgl/WebGL2RenderingContext.cpp
@@ -340,7 +340,7 @@ ScriptValue WebGL2RenderingContext::getUniform(WebGLProgram* program,
         return scriptNull();
     }
 
-    if (location->program()->context() != this) {
+    if (!isFromCurrentContext(location->program()) {
         setGLError(GL_INVALID_OPERATION);
         return scriptNull();
     }
