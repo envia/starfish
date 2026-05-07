7cb759e0acfa2eef16bc21ea9c466cf5a70ba240
Joonhyung Hwang <jh1984.hwang@samsung.com>
2026-05-07 17:41:24 +0900

    Implement WebGL2 buffer objects
    Implement bufferData(), bufferSubData(), getBufferParameter(),
    copyBufferSubData() and getBufferSubData() of WebGL2RenderingContext.
    In addition, update getBufferParameter() of WebGLRenderingContext
    to check the type of the target, for gl-object-get-calls.html.
    
    We need to add checking of the currently active transform feedback
    object to getBufferSubData() when implementing transform feedback
    objects.
    
    Signed-off-by: Joonhyung Hwang <jh1984.hwang@samsung.com>

--- src/platform/canvas/gl/GL.h
+++ src/platform/canvas/gl/GL.h
@@ -335,6 +335,8 @@ public:
     virtual void getInteger64v(GLenum pname, GLint64 *data) = 0;
     virtual void getSynciv(GLsync sync, GLenum pname, GLsizei bufSize,
                            GLsizei *length, GLint *values) = 0;
+    virtual void getBufferParameteri64v(GLenum target, GLenum pname,
+                                        GLint64 *params) = 0;
 };
 
 } // namespace Starfish
